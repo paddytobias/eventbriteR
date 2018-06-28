3fde0b8e7501a55d1b841e745a98db43835c90ef
PTobias <paddytobias@me.com>
2018-06-28 20:52:40 +1000

    first commit

--- R/call_eventbrite_function.R
+++ R/call_eventbrite_function.R
@@ -1,5 +1,5 @@
 #' Call Eventbrite
-#' 
+#'
 #' This function compiles the url required to call the Eventbrite API based on a few parameters.
 #' call_eventbrite returns the URL as a string, which should be used with get_eb_event()
 #' function relies on "httr" package and will install if it doesn't exist
@@ -8,8 +8,8 @@
 #' @param sub_query the particular information wanted from the query. If query = "events", then sub_query could be "attendees", "users", "orders", "display_settings", "ticket_classes", "canned_questions", "questions", "discounts", "public_discounts", "access_codes", "transfers", "teams", "ticket_groups", "ticket_classes", "ticket_buyer_settings"
 #' @param token the API token needed to call the API. Go here to create your own app and get an API token: http://www.eventbrite.com/myaccount/apps/
 #' @import "httr"
-#' @export '.'
-#' @examples 
+#' @export
+#' @examples
 #' url = call_eventbrite(event_id = event_id, sub_query = "orders", token = token) # will return the url for a given event's orders/registrations (using event_id)
 #' event = get_eb_event(url)
 #' orders = get_eb_orders(event)
@@ -24,17 +24,17 @@ call_eventbrite = function(event_id=NA, query="events", sub_query=NA, second_id=
     warning("Please enter API token")
   }
   params = paste0("?token=",token)
-  
+
   possible_first = c("events", "users", "orders")
   if (query %in% possible_first){
     first = paste0(query, "/")
   } else {
     stop("Not a valid first API call: ", query)
   }
-  
-  possible_second= c("attendees", "users", "orders", "display_settings", 
-                     "ticket_classes", "canned_questions", "questions", 
-                     "discounts", "public_discounts", "access_codes", 
+
+  possible_second= c("attendees", "users", "orders", "display_settings",
+                     "ticket_classes", "canned_questions", "questions",
+                     "discounts", "public_discounts", "access_codes",
                      "transfers", "teams", "ticket_groups",
                      "ticket_classes", "ticket_buyer_settings")
   if (query=="events" & sub_query %in% possible_second){
@@ -42,31 +42,31 @@ call_eventbrite = function(event_id=NA, query="events", sub_query=NA, second_id=
   } else {
     stop("Not a valid second API call: ", sub_query)
   }
-  
+
   if (!is.na(first) & is.na(event_id)){
     stop("No id inserted for: ", first)
   } else {
     event_id = paste0(event_id, "/")
   }
-  
+
   if (!is.na(second_id)){
     second_id = paste0(second_id, "/")
   } else {
     second_id =""
   }
-  
+
   url = paste0(baseURL, first, event_id, second, second_id, params)
-  
+
   message("Calling ", url)
   req <- GET(url)
-  
+
   if(req$status_code != 200){
     stop("Problem with calling the API - response: ", "\n", content(req)[3], ": ",content(req)[2])
-  } 
-  
-  
+  }
+
+
   return(url)
-  
+
 }
 
 
