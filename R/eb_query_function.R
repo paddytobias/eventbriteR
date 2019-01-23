4bce4c221d8a9160398ef6ed65ed23442cc5098b
Paddy Tobias <paddy.tobias@deakin.edu.au>
2019-01-23 16:44:29 +1100

    restructuring

--- R/eb_query_function.R
+++ R/eb_query_function.R
@@ -1,8 +1,11 @@
 #' Get Eventbrite object
 #'
-#' This function compiles extracts the Eventbrite's event data from its original JSON form.
-#' function relies on "jsonlite" package and will install if it doesn't exist
-#' @param url the Eventbrite API url to call. This URL can be generated with call_eventbrite
+#' This function is a wrapper for call_eventbrite to simplify getting data after the url has been tested to work. It extracts the Eventbrite's event data from its original JSON format.
+#' The function relies on "jsonlite" package and will install if it doesn't exist
+#' @param query_id the unique id that every Eventbrite event has
+#' @param query the top-level query. Defaults to "events". Other query entries could be "users" or "orders".
+#' @param sub_query the particular information wanted from the query. If query = "events", then sub_query could be "attendees", "users", "orders", "display_settings", "ticket_classes", "canned_questions", "questions", "discounts", "public_discounts", "access_codes", "transfers", "teams", "ticket_groups", "ticket_classes", "ticket_buyer_settings"
+#' @param token the API token needed to call the API. Go here to create your own app and get an API token: http://www.eventbrite.com/myaccount/apps/
 #' @import "jsonlite"
 #' @export
 #' @examples
